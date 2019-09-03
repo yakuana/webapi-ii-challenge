9f6852107c5dc72817f74eadd397a84c47c6bdee
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-09-03 17:18:33 -0400

    Complete.

--- server.js
+++ server.js
@@ -115,6 +115,56 @@ server.get('/api/posts/:id/comments', (req, res) => {
     }
 })
 
+// DELETE /api/posts/:id
+server.delete('/api/posts/:id', (req, res) => {
+    const { id } = req.params; // OR const id = req.params.id;
+
+    if (id) {
+        db.remove(id) 
+        .then(post => {
+            res.status(200).json(post)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "The post could not be removed",
+                error: err
+            })
+        })
+    } else {
+        res.status(404).json({
+            message: "The post with the specified ID does not exist.",
+        })
+    }
+})
+
+// PUT /api/posts/:id
+server.put('/api/posts/:id', (req, res) => {
+    const { id } = rew.params; 
+    const editPost = req.body;
+
+    if (id) {
+        if (editPost.title && editPost.contents) {
+            db.insertComment(editPost)
+            .then(post => {
+                res.status(200).json(post)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: "The post information could not be modified.",
+                    error: err
+                })
+            })
+        } else {
+            res.status(400).json({
+                error: "Please provide text for the comment."
+            })
+        }
+    } else {
+        res.status(404).json({
+            message: "The post with the specified ID does not exist.",
+        })
+    }
+})
 
 // export server 
 module.exports = server 
